30b014d068c2acefeb39b96edc6e04129d8727e7
GoJaeHak <goddkfvlwl@gmail.com>
2018-11-28 11:08:01 +0900

    두번째 버튼 추가

--- GitEx1/Form1.cs
+++ GitEx1/Form1.cs
@@ -21,5 +21,10 @@ namespace GitEx1
         {
             MessageBox.Show("Test");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test2");
+        }
     }
 }
